24aaf5400f770cff96c947c6047fa396ea9ec8ae
Ar0cka <120028878+Ar0cka@users.noreply.github.com>
2023-08-13 04:19:15 +1200

    add player
    Задумка на утро, сделать доход

--- Empire of Opportunities/Assets/Scripts/Money.cs
+++ Empire of Opportunities/Assets/Scripts/Money.cs
@@ -6,17 +6,11 @@ using UnityEngine;
 public class Money : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMeshpro;
-    private int money = 300;
     private string text = "Money";
-
-    private void FixedUpdate()
-    {
-        money += 2;     
-    }
 
     private void Update()
     {
-       textMeshpro.text = text + "" +  money;
+       textMeshpro.text = text + "";
     }
 
 
